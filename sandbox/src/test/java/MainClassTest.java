ad5c4249334f4892e3fd442130e311a1219257ee
Elena Filiptsova <elena_filiptsova@relex.ru>
2019-08-23 18:41:00 +0300

    Добавлено задание "Test2"

--- sandbox/src/test/java/MainClassTest.java
+++ sandbox/src/test/java/MainClassTest.java
@@ -2,10 +2,15 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MainClassTest {
+  private MainClass mc = new MainClass();
 
   @Test
   public void testGetLocalNumber() {
-    MainClass mc = new MainClass();
     Assert.assertTrue("getLocalNumber() is not equal 14", mc.getLocalNumber() == 14);
+  }
+
+  @Test
+  public void testGetClassNumber() {
+    Assert.assertTrue("getClassNumber() is more than 45", mc.getClassNumber() > 45);
   }
 }
