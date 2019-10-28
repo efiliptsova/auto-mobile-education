6a1b99c443c6a8ddc986e55d349b90ddf93a1a74
Elena <elena_filiptsova@relex.ru>
2019-10-28 19:26:36 +0300

    В процессе работы над заданием 10 - добавлены локаторы для welcome-экранов

--- javaAppiumAutomation/src/lib/ui/WelcomePageObject.java
+++ javaAppiumAutomation/src/lib/ui/WelcomePageObject.java
@@ -7,6 +7,10 @@ public class WelcomePageObject extends MainPageObject{
 
   private static final String
           WELCOME_SCREEN1 = "id:Learn more about Wikipedia";
+  WELCOME_SCREEN2 = "id:New ways to explore";
+  WELCOME_SCREEN3 = "id:Add or edit preferred languages";
+  WELCOME_SCREEN4 = "Learn more about data collected";
+  GET_STARTED = "id:Get started";
 
   public WelcomePageObject(AppiumDriver driver) {
     super(driver);
@@ -15,4 +19,10 @@ public class WelcomePageObject extends MainPageObject{
   public void waitForWelcomeScreen1() {
     this.waitForElementAndClick(WELCOME_SCREEN1, "Connot find 'Learn more about Wikipedia'", 10);
   }
+
+    public void clickNext() {
+        this.waitForElementAndClick("id:Next", "Connot find 'Learn more about Wikipedia'", 10);
+    }
+
+
 }
